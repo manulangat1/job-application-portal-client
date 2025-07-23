d11cc41d53709f0b73bc3a28768989fe9840ca6c
manulangat1 <emmanuelthedeveloper@gmail.com>
2025-07-23 11:53:10 +0300

    [chore] uses correct format in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   allowedHosts: ["all"],
-  // },
   server: {
-    host: true,
+    host: true, // allows external access
+    allowedHosts: ["jobs.kipchirchirlangat.com", "client", "localhost"],
   },
 });
