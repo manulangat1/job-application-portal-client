706112c56c9f75a6d4bd2fba71907b404bc5e6c7
manulangat1 <emmanuelthedeveloper@gmail.com>
2025-07-23 11:23:12 +0300

    [chore] revert latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // needed to allow external access
-    allowedHosts: ["jobs.kipchirchirlangat.com"], // <-- Add your domain here
-  },
+  // server: {
+  //   host: true, // needed to allow external access
+  //   // allowedHosts: "all",
+  //   allowedHosts: ["all"],
+  //   // allowedHosts: ["jobs.kipchirchirlangat.com"], // <-- Add your domain here
+  // },
 });
